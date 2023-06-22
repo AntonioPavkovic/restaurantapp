19820e589056e8bc534dd9e11b5f580a4e7b2957
AntonioPavkovic <antoniopavkovic999@gmail.com>
2023-06-22 20:21:50 +0200

    finished project

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { MdOutlineKeyboardBackspace } from "react-icons/md";
+import { MdOutlineKeyboardBackspace, MdClose } from "react-icons/md";
 import { RiRefreshFill } from "react-icons/ri";
-
 import { motion } from "framer-motion";
 import { useStateValue } from "../context/StateProvider";
 import { actionType } from "../context/reducer";
@@ -12,6 +11,12 @@ const CartContainer = () => {
   const [{ cartShow, cartItems, user }, dispatch] = useStateValue();
   const [flag, setFlag] = useState(1);
   const [tot, setTot] = useState(0);
+  const [showForm, setShowForm] = useState(false);
+  const [orderSuccess, setOrderSuccess] = useState(false);
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [formError, setFormError] = useState("");
 
   const showCart = () => {
     dispatch({
@@ -25,8 +30,7 @@ const CartContainer = () => {
       return accumulator + item.qty * item.price;
     }, 0);
     setTot(totalPrice);
-    console.log(tot);
-  }, [tot, flag]);
+  }, [cartItems]);
 
   const clearCart = () => {
     dispatch({
@@ -35,6 +39,32 @@ const CartContainer = () => {
     });
 
     localStorage.setItem("cartItems", JSON.stringify([]));
+  };
+
+  const handleCheckout = () => {
+    setShowForm(true);
+  };
+
+  const handleOrderPlacement = () => {
+    if (!name || !phoneNumber || !address) {
+      setFormError("Please fill out all fields");
+    } else {
+      // Perform order placement logic here (e.g., saving the order to a database)
+
+      // Clear the cart items
+      dispatch({
+        type: actionType.SET_CARTITEMS,
+        cartItems: [],
+      });
+
+      // Display success message
+      setOrderSuccess(true);
+      setShowForm(false);
+    }
+  };
+
+  const handleOrderSuccessClose = () => {
+    setOrderSuccess(false);
   };
 
   return (
@@ -52,77 +82,121 @@ const CartContainer = () => {
 
         <motion.p
           whileTap={{ scale: 0.75 }}
-          className="flex items-center gap-2 p-1 px-2 my-2 bg-gray-100 rounded-md hover:shadow-md  cursor-pointer text-textColor text-base"
+          className="flex items-center gap-2 p-1 px-2 my-2 bg-gray-100 rounded-md hover:shadow-md cursor-pointer text-textColor text-base"
           onClick={clearCart}
         >
           Clear <RiRefreshFill />
         </motion.p>
       </div>
 
-      {/* bottom section */}
-      {cartItems && cartItems.length > 0 ? (
-        <div className="w-full h-full bg-cartBg rounded-t-[2rem] flex flex-col">
-          {/* cart Items section */}
-          <div className="w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-none">
-            {/* cart Item */}
-            {cartItems &&
-              cartItems.length > 0 &&
-              cartItems.map((item) => (
-                <CartItem
-                  key={item.id}
-                  item={item}
-                  setFlag={setFlag}
-                  flag={flag}
-                />
-              ))}
+      {showForm ? (
+        <div className="w-full h-full bg-gray-900 bg-opacity-75 fixed top-0 left-0 flex items-center justify-center">
+          <div className="bg-white p-8 rounded-md relative">
+            <motion.div
+              whileTap={{ scale: 0.75 }}
+              className="absolute top-4 right-4 cursor-pointer"
+              onClick={() => setShowForm(false)}
+            >
+              <MdClose className="text-gray-500 text-2xl" />
+            </motion.div>
+            <h2 className="text-2xl font-semibold mb-4">Order Information</h2>
+            {/* Form fields for name, phone number, and address */}
+            {/* Replace with your own form implementation */}
+            <input
+              type="text"
+              placeholder="Name"
+              className="mb-4"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Phone Number"
+              className="mb-4"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Address"
+              className="mb-4"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+
+            <div className="flex items-center justify-between">
+              <p className="text-lg">Price to Pay:</p>
+              <p className="text-lg font-semibold">${tot + 2.5}</p>
+            </div>
+
+            <button
+              type="button"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+              onClick={handleOrderPlacement}
+            >
+              Place Order
+            </button>
+            {formError && <p className="text-red-500">{formError}</p>}
           </div>
-
-          {/* cart total section */}
-          <div className="w-full flex-1 bg-cartTotal rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2">
-            <div className="w-full flex items-center justify-between">
-              <p className="text-gray-400 text-lg">Sub Total</p>
-              <p className="text-gray-400 text-lg">$ {tot}</p>
-            </div>
-            <div className="w-full flex items-center justify-between">
-              <p className="text-gray-400 text-lg">Delivery</p>
-              <p className="text-gray-400 text-lg">$ 2.5</p>
-            </div>
-
-            <div className="w-full border-b border-gray-600 my-2"></div>
-
-            <div className="w-full flex items-center justify-between">
-              <p className="text-gray-200 text-xl font-semibold">Total</p>
-              <p className="text-gray-200 text-xl font-semibold">
-                ${tot + 2.5}
-              </p>
-            </div>
-
-            {user ? (
-              <motion.button
-                whileTap={{ scale: 0.8 }}
-                type="button"
-                className="w-full p-2 rounded-full bg-gradient-to-tr from-orange-400 to-orange-600 text-gray-50 text-lg my-2 hover:shadow-lg"
-              >
-                Check Out
-              </motion.button>
-            ) : (
-              <motion.button
-                whileTap={{ scale: 0.8 }}
-                type="button"
-                className="w-full p-2 rounded-full bg-gradient-to-tr from-orange-400 to-orange-600 text-gray-50 text-lg my-2 hover:shadow-lg"
-              >
-                Login to check out
-              </motion.button>
-            )}
+        </div>
+      ) : orderSuccess ? (
+        <div className="w-full h-full bg-gray-900 bg-opacity-75 fixed top-0 left-0 flex items-center justify-center">
+          <div className="bg-white p-8 rounded-md">
+            <motion.div
+              whileTap={{ scale: 0.75 }}
+              className="absolute top-4 right-4 cursor-pointer"
+              onClick={handleOrderSuccessClose}
+            >
+              <MdClose className="text-gray-500 text-2xl" />
+            </motion.div>
+            <h2 className="text-2xl font-semibold mb-4">Order Successfully Placed!</h2>
+            <p>Your order has been placed successfully.</p>
           </div>
         </div>
       ) : (
-        <div className="w-full h-full flex flex-col items-center justify-center gap-6">
-          <img src={EmptyCart} className="w-300" alt="" />
-          <p className="text-xl text-textColor font-semibold">
-            Add some items to your cart
-          </p>
-        </div>
+        <React.Fragment>
+          {cartItems && cartItems.length > 0 ? (
+            <div className="w-full h-full bg-cartBg rounded-t-[2rem] flex flex-col">
+              {/* cart Items section */}
+              <div className="w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-none">
+                {/* cart Item */}
+                {cartItems &&
+                  cartItems.length > 0 &&
+                  cartItems.map((item) => (
+                    <CartItem key={item.id} item={item} setFlag={setFlag} flag={flag} />
+                  ))}
+              </div>
+
+              {/* cart total section */}
+              <div className="w-full flex-1 bg-cartTotal rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2">
+                <div className="w-full flex items-center justify-between">
+                  <p className="text-gray-400 text-lg">Sub Total</p>
+                  <p className="text-gray-400 text-lg">$ {tot}</p>
+                </div>
+                <div className="w-full flex items-center justify-between">
+                  <p className="text-gray-400 text-lg">Delivery</p>
+                  <p className="text-gray-400 text-lg">$ 2.50</p>
+                </div>
+                <div className="w-full flex items-center justify-between">
+                  <p className="text-lg font-semibold">Total</p>
+                  <p className="text-lg font-semibold">$ {tot + 2.5}</p>
+                </div>
+                <button
+                  type="button"
+                  className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+                  onClick={handleCheckout}
+                >
+                  Checkout
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div className="flex flex-col items-center justify-center h-full">
+              <img src={EmptyCart} alt="empty-cart" className="h-44" />
+              <p className="text-2xl text-gray-400 mt-8">Your cart is empty!</p>
+            </div>
+          )}
+        </React.Fragment>
       )}
     </motion.div>
   );
